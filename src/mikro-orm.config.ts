28f6b172b4c98276ca40876aa18db0723b8bbb7e
Semilore Idowu <semijacks27@gmail.com>
2021-06-16 18:32:27 +0100

    :sparkles: initial commit & setup MikroORM

---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,19 @@
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import dotenv from "dotenv";
+import { MikroORM } from "@mikro-orm/core";
+import path from "path";
+
+dotenv.config();
+
+export default {
+  migrations: {
+    path: path.join(__dirname, "./migrations"),
+    pattern: /^[\w-]+\d+\.[tj]s$/,
+  },
+  entities: [Post],
+  dbName: "lireddit",
+  type: "postgresql",
+  debug: !__prod__,
+  password: process.env.POSTGRES_PASSWORD,
+} as Parameters<typeof MikroORM.init>[0];
