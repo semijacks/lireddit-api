49230f88a2d590269d9a7062f032f0d75fa4ed8c
Semilore Idowu <semijacks27@gmail.com>
2021-07-09 17:45:53 +0100

    :sparkles: added register and login

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
-import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
+import { UserResolver } from "./resolvers/user";
+import { __prod__ } from "./constants";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
@@ -14,9 +17,10 @@ const main = async () => {
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver],
+      resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
+    context: () => ({ em: orm.em }),
   });
 
   apolloServer.applyMiddleware({ app });
